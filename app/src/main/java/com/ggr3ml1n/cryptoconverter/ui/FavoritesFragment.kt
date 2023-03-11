d2cde2cb970722397e6267473e8a40fd361cafd7
Dinar Subeev <dinars163@gmail.com>
2023-03-12 01:15:53 +0400

    Подключен viewBinding

--- app/src/main/java/com/ggr3ml1n/cryptoconverter/ui/FavoritesFragment.kt
+++ app/src/main/java/com/ggr3ml1n/cryptoconverter/ui/FavoritesFragment.kt
@@ -1,19 +1,27 @@
 package com.ggr3ml1n.cryptoconverter.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.ggr3ml1n.cryptoconverter.R
+import androidx.fragment.app.Fragment
+import com.ggr3ml1n.cryptoconverter.databinding.FragmentFavoritesBinding
 
 class FavoritesFragment : Fragment() {
+
+    private var _binding: FragmentFavoritesBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_favorites, container, false)
+    ): View {
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
     companion object {
