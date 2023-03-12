6702adbdf651df1c28fc3614912e8ce61626e429
Dinar Subeev <dinars163@gmail.com>
2023-03-12 23:54:35 +0400

    Инициализирован адаптер, добавлен тестовый объект в RCV

--- app/src/main/java/com/ggr3ml1n/cryptoconverter/ui/FavoritesFragment.kt
+++ app/src/main/java/com/ggr3ml1n/cryptoconverter/ui/FavoritesFragment.kt
@@ -5,12 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ggr3ml1n.cryptoconverter.R
+import com.ggr3ml1n.cryptoconverter.adapters.FavoritesAdapter
 import com.ggr3ml1n.cryptoconverter.databinding.FragmentFavoritesBinding
+import com.ggr3ml1n.cryptoconverter.dataclasses.CryptoModel
 
 class FavoritesFragment : Fragment() {
 
     private var _binding: FragmentFavoritesBinding? = null
     private val binding get() = _binding!!
+
+    private var adapter = FavoritesAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,7 +27,23 @@ class FavoritesFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init(){
+        binding.rcFavorites.layoutManager = LinearLayoutManager(requireContext())
+        binding.rcFavorites.adapter = adapter
+
+        //Тут я добавил тестовую модельку для того, чтобы посмотреть как она отображается на RCView
+        adapter.add(CryptoModel(
+            R.drawable.sample_icon,
+            "BTC",
+            "Bitcoin",
+            R.drawable.sample_graphic,
+            "$ 29850.15",
+            "1.12412 BTC",
+            "54.21%"
+        ))
     }
 
     companion object {
